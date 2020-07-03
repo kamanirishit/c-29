44b517fa57d21877916bcf422c6fe5358216dab1
kamanirishit <66833747+kamanirishit@users.noreply.github.com>
2020-07-03 18:00:36 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,12 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(0.3,x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+  }
+}
---
+++ Pig.js
@@ -0,0 +1,7 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(0.4,x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+  }
+
+};
